c272ae137ee78378848644414d7cb7008b7bcc48
Phillip <pcampbe8@asu.edu>
2024-09-18 08:56:14 -0700

    API bookmark was added (not functional)

--- yarnia-project/src/API/index.js
+++ yarnia-project/src/API/index.js
@@ -124,7 +124,7 @@ export async function fetchWithAuth(url, options = {}) {
 
 export const updateStoryContent = async (storyId, content) => {
   try {
-    const response = await fetch(`/api/stories/${storyId}`, {
+    const response = await fetch(`${API_URL}/api/stories/${storyId}`, {
       method: "PUT", // Or PATCH, depending on your API design
       headers: {
         "Content-Type": "application/json",
@@ -141,3 +141,44 @@ export const updateStoryContent = async (storyId, content) => {
     console.error("Error updating story content:", error);
   }
 };
+
+
+// Girl who knows (bookmark stuff)
+
+export async function fetchBookmarkedStories() {
+  try {
+    const response = await fetch(`${API_URL}/api/${authorId}/bookmarks/`);
+    //  check the response for validity in the fetch statement
+    if (!response.ok) {
+      throw new Error(`Error fetching story: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching user bookmarks", error);
+    throw error;
+  }
+}
+
+export async function removeBookmark(storyId) {
+  try {
+    const response = await fetch(`${API_URL}/api/${authorId}/bookmarks/`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to remove bookmark.");
+    }
+
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error("Failed to remove the bookmark.")
+  }
+}
+
+// girl who knows
