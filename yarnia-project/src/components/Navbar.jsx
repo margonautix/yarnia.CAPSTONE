58a795f4a471cd7b43aa418593db4d3915d13efd
Margo <giddens.margo@gmail.com>
2024-09-22 13:20:42 -0500

    syntax

--- yarnia-project/src/components/Navbar.jsx
+++ yarnia-project/src/components/Navbar.jsx
@@ -38,14 +38,12 @@ const NavBar = ({ user, setUser }) => {
           <Link to="/">Home</Link>
         </li>
 
-
         {/* Conditionally render the Bookmarks link based on user login status */}
         {user && (
           <li>
             <Link to="/bookmarks">Bookmarks</Link>
           </li>
         )}
-
 
         {/* Conditionally render links based on user login status */}
         {user ? (
@@ -58,11 +56,6 @@ const NavBar = ({ user, setUser }) => {
             </li>
             {/* Keep Logout as a link but add onClick to handle logout */}
             <li>
-              <Link
-                to="/logout"
-                onClick={handleLogout}
-                style={{ marginLeft: "auto" }}
-              >
               <Link
                 to="/logout"
                 onClick={handleLogout}
@@ -85,7 +78,6 @@ const NavBar = ({ user, setUser }) => {
       </ul>
     </nav>
   );
-};
 };
 
 export default NavBar;
